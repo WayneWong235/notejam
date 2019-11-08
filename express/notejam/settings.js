957f8ab06645c25c6c054347044f2a0726ae8f55
Wayne Wong <waynewong@m-daq.com>
2019-11-08 23:49:05 +0800

    update

--- express/notejam/settings.js
+++ express/notejam/settings.js
@@ -2,20 +2,20 @@ var settings = {
   development: {
     db: {
       host: "demo.ctotioexeoyh.ap-southeast-1.rds.amazonaws.com",
-      user: "admin",
-      password: "Waynewong24.",
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWD,
       database: "demo"
     },
-    dsn: "mysql://admin:Waynewong24.@demo.ctotioexeoyh.ap-southeast-1.rds.amazonaws.com/demo"
+    dsn: "mysql://"+process.env.DB_USERNAME+":"+process.env.DB_PASSWD+"@demo.ctotioexeoyh.ap-southeast-1.rds.amazonaws.com/demo"
   },
   test: {
     db: {
       host: "demo.ctotioexeoyh.ap-southeast-1.rds.amazonaws.com",
-      user: "admin",
-      password: "Waynewong24.",
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWD,
       database: "demo"
     },
-    dsn: "mysql://admin:Waynewong24.@demo.ctotioexeoyh.ap-southeast-1.rds.amazonaws.com/demo"
+    dsn: "mysql://"+process.env.DB_USERNAME+":"+process.env.DB_PASSWD+"@demo.ctotioexeoyh.ap-southeast-1.rds.amazonaws.com/demo"
   }
 };
 
